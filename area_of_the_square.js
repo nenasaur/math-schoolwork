7758d4d8e0a2ab75dad736cd6f6a057d8dc93bcf
Ian S. Molina <iansouzamolina@gmail.com>
2025-05-22 23:24:32 -0300

    area_of_the_square.js

---
+++ area_of_the_square.js
@@ -0,0 +1,19 @@
+// Área de um quadrado
+
+
+//---------------------------------------------------------------------------------------------------------------
+/* para calcular a a área de um quadrado basta saber o valor de uma das suas laterais elevar ao quadrado 
+
+variáveis nesse código :
+
+l = lado do quadrado
+
+a = área do quadrado 
+
+//-------------------------------------------------------------------------------------------------------------*/
+
+l = prompt("digite o valor do lado do quadrado ")
+
+a = l**2
+
+alert("A área do quadrado  é : " +a)
