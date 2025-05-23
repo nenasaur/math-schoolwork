05311c9dde51af10decd3bf229c9468bdcf7ec2a
Ian S. Molina <iansouzamolina@gmail.com>
2025-05-22 23:24:09 -0300

    rhombus_area.js

---
+++ rhombus_area.js
@@ -0,0 +1,20 @@
+/* Área do Losângulo
+                    Ian S. Molina
+
+//------------------------------------------
+A fórmula para calcular a área de um 
+losângulo é a diagonal maior multiplicada pela diagonal menor divida por dois.
+
+onde :
+
+D = diagonal maior 
+d = diagonal menor
+//--------------------------------------------*/
+
+
+D = prompt("diagonal maior")
+d = prompt("diagonal menor")
+
+a=parseFloat (D*d)/2
+
+alert("A Área do losângulo é : " +a)
