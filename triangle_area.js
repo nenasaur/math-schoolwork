324afd698a6e861c6fac3c3d079fec6a89412d21
Ian S. Molina <iansouzamolina@gmail.com>
2025-05-22 23:21:59 -0300

    triangle_area.js

---
+++ triangle_area.js
@@ -0,0 +1,27 @@
+/*  calculadora de área de um triângulo
+                            Ian S. Molina
+
+//------------------------------------------
+A fórmula para calcular a área de um 
+triângulo é base vezes altura dividido 
+por dois.  
+
+onde :
+
+h = altura do triângulo
+b = base do triângulo
+//--------------------------------------------*/
+
+
+h = prompt("altura")
+b= prompt("base")
+
+a =parseFloat (b*h)/2
+
+alert("A área do triângulo  é : " +a)
+
+
+
+
+
+//        atualizado 16/11/2024 às 10:11h
