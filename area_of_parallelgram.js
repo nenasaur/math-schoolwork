89b2ac452d9b134f46533d42ced24776a5852266
Ian S. Molina <iansouzamolina@gmail.com>
2025-05-22 23:23:19 -0300

    area_of_parallelgram.js

---
+++ area_of_parallelgram.js
@@ -0,0 +1,18 @@
+//área de um paralelogramo//
+
+/*--------------------------------------------------------------------------------
+
+Para calcular a área de um paralelogramo é simples, apenas base vezes a altura.
+onde ;
+
+h = altura
+b = base
+
+-------------------------------------------------------------------------------*/
+
+b =prompt("digite a medida da base")
+h=prompt("digite a medida da altura")
+
+a=parseFloat(b*h);
+
+alert("a área do retângulo é" + a)
